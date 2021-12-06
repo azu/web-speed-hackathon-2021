1c9f9163eba52062473cd9a6c8622ad1ae9ce70e
azu <azuciao@gmail.com>
2021-12-06 23:36:57 +0900

    fix: webpack env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,6 @@ const config = {
     },
     static: [PUBLIC_PATH, UPLOAD_PATH],
   },
-  devtool: 'inline-source-map',
   entry: {
     main: [
       'jquery-binarytransport',
@@ -29,7 +28,6 @@ const config = {
       path.resolve(SRC_PATH, './index.jsx'),
     ],
   },
-  mode: 'none',
   module: {
     rules: [
       {
@@ -62,7 +60,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: process.env.NODE_ENV,
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
