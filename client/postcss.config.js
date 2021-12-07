3feffa7c942bb64de1907c7107b36d4b0b83417b
azu <azuciao@gmail.com>
2021-12-08 00:53:04 +0900

    cssnano minify
    fix https://github.com/azu/web-speed-hackathon-2021/issues/20

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,13 +1,16 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
 const tailwindcss = require("tailwindcss");
-
+const cssnano = require("cssnano");
 module.exports = {
     plugins: [
         postcssImport(),
         tailwindcss(),
         postcssPresetEnv({
             stage: 3
+        }),
+        cssnano({
+            preset: "default"
         })
     ]
 };
