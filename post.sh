83024d745362cd3007ae3c357d0b5a6fa09c1197
azu <azuciao@gmail.com>
2021-12-07 23:36:05 +0900

    enable gzip sh

--- post.sh
+++ post.sh
@@ -6,6 +6,9 @@ BASE_DIR="$(cd "$(dirname "$0")"; pwd)";
 BUILD_DIR="$BASE_DIR/dist"
 PUBLIC_DIR="$BASE_DIR/public"
 
-for file in `find $BUILD_DIR -name "*.js" -name "*.css"`; do
-  gzip -k -9 $file
+for file in `find $BUILD_DIR -name "*.js"`; do
+  gzip -k -9 -f $file
+done
+for file in `find $BUILD_DIR -name "*.css"`; do
+  gzip -k -9 -f $file
 done
