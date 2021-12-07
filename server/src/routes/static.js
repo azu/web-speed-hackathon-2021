83cf371cb3d6eb4e6bd713a880a9b0ad7fd7510c
azu <azuciao@gmail.com>
2021-12-07 23:34:07 +0900

    enable gzip

--- server/src/routes/static.js
+++ server/src/routes/static.js
@@ -1,48 +1,57 @@
 import history from "connect-history-api-fallback";
 import Router from "express-promise-router";
+import staticServe from "serve-static";
 import expressStaticGzip from "express-static-gzip";
 import { CLIENT_DIST_PATH, PUBLIC_PATH, UPLOAD_PATH } from "../paths";
 
+const IS_DEV = process.env.NODE_ENV !== "production";
 const router = Router();
 
 // SPA 対応のため、ファイルが存在しないときに index.html を返す
 router.use(history());
 
 router.use(
-    expressStaticGzip(UPLOAD_PATH, {
-        enableBrotli: true,
-        serveStatic: {
-            etag: true,
-            lastModified: true,
-            immutable: true,
-            maxAge: 36 * 1000 * 10
-        }
-    })
+    IS_DEV
+        ? staticServe(UPLOAD_PATH)
+        : expressStaticGzip(UPLOAD_PATH, {
+              enableBrotli: true,
+              serveStatic: {
+                  etag: true,
+                  lastModified: true,
+                  immutable: true,
+                  maxAge: 3600 * 1000
+              }
+          })
 );
 
 router.use(
-    expressStaticGzip(PUBLIC_PATH, {
-        enableBrotli: true,
-        serveStatic: {
-            etag: true,
-            lastModified: true,
-            immutable: true,
-            maxAge: 36 * 1000 * 10,
-            cacheControl: false
-        }
-    })
+    IS_DEV
+        ? staticServe(PUBLIC_PATH)
+        : expressStaticGzip(PUBLIC_PATH, {
+              enableBrotli: true,
+              serveStatic: {
+                  etag: true,
+                  lastModified: true,
+                  immutable: true,
+                  maxAge: 3600 * 1000,
+                  cacheControl: false
+              }
+          })
 );
 
 router.use(
-    expressStaticGzip(CLIENT_DIST_PATH, {
-        enableBrotli: true,
-        serveStatic: {
-            etag: true,
-            lastModified: true,
-            immutable: true,
-            maxAge: 36 * 1000 * 10
-        }
-    })
+    IS_DEV
+        ? staticServe(CLIENT_DIST_PATH)
+        : expressStaticGzip(CLIENT_DIST_PATH, {
+              enableBrotli: true,
+              serveStatic: {
+                  etag: true,
+                  lastModified: true,
+                  immutable: true,
+                  maxAge: 3600 * 1000,
+                  cacheControl: false
+              }
+          })
 );
 
 export { router as staticRouter };
