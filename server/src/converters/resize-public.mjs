02254ecf9357048c88c1b9fc1d9ea6aa3175973b
azu <azuciao@gmail.com>
2021-12-07 09:44:16 +0900

    1024

--- server/src/converters/resize-public.mjs
+++ server/src/converters/resize-public.mjs
@@ -29,7 +29,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const images = await globby(join(__dirname, "../../../public/images") +"**/*.jpg")
 const all = images.map(async image => {
     return convertImage(await fs.readFile(image), {
-        width: 1280
+        height: 1080
     }).then(async resizeBuffer => {
         await fs.writeFile(image, resizeBuffer);
     })
