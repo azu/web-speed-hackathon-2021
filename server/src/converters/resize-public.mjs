8a6c3f76f6161e43ba1be640d5e3d7b37acd78fe
azu <azuciao@gmail.com>
2021-12-07 09:36:26 +0900

    width

--- server/src/converters/resize-public.mjs
+++ server/src/converters/resize-public.mjs
@@ -6,6 +6,8 @@ import { convertImage} from "./convert_image.js"
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const images = await globby(join(__dirname, "../../../public/images") +"**/*.jpg")
 const all = images.map(async image => {
-    return convertImage(await fs.readFile(image));
+    return convertImage(await fs.readFile(image), {
+        width: 2000
+    });
 });
 await Promise.all(all);
