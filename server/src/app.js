eeef35d1e6233a53816bc4c4598c276cf1efae29
azu <azuciao@gmail.com>
2021-12-08 10:43:18 +0900

    feat: gzip response

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,26 @@
 import bodyParser from "body-parser";
 import Express from "express";
 import session from "express-session";
-
+import compression from "compression";
 import { apiRouter } from "./routes/api";
+
 import { staticRouter } from "./routes/static";
 
 const app = Express();
 
 app.set("trust proxy", true);
 
+app.use(compression());
 app.use(
     session({
         proxy: true,
         resave: false,
         saveUninitialized: false,
-        secret: "secret"
+        secret: "poales-queak-ree-crin-carisa-tiler-frijol"
     })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: "10mb" }));
-
-app.use((_req, res, next) => {
-    return next();
-});
-
 app.use("/api/v1", apiRouter);
 app.use(staticRouter);
 
